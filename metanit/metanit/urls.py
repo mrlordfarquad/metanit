8b3b9958e4c1eea88171ff252ee7c82913f34f1b
mrlordfarquad <144800480+mrlordfarquad@users.noreply.github.com>
2023-11-20 15:10:06 +0600

    Delete metanit directory

--- metanit/metanit/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from metanit.views import PersonDetailView
-
-urlpatterns = [
-    path('person/<int:person_id>/', PersonDetailView.as_view(), name='person-detail'),
-]
